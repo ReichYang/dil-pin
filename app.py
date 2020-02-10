143b871b68c618a7864ca3fb56ae4709ce84d4ba
ReichYang <yukunyang9508@gmail.com>
2020-02-10 15:00:12 -0500

    0210

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import flask
-from flask import Flask, redirect, url_for, request,session
+from flask import Flask, redirect, url_for, request,session, jsonify
 from datetime import datetime
 import re, sys,os, json
 from flask import render_template
@@ -11,9 +11,6 @@ from flask_fontawesome import FontAwesome
 app = Flask(__name__)
 app.secret_key = os.urandom(12)
 
-# @app.route('/success/<name>')
-# def success(name):
-#     return 'welcome %s' % name
 
 
 @app.route("/")
@@ -27,13 +24,13 @@ def login(data=None):
         user = request.form['username']
         email=request.form['email']
         pw = request.form['password']
-        print(user, file=sys.stdout)
+        # print(user, file=sys.stdout)
         pint = Pinterest.Pinterest(username=user,email=email, password=pw)
         flask.current_app.account= pint
         login_sts= pint.login()
-        print(login_sts.status_code, file=sys.stdout)
-        print(pint.get_user_overview(), file=sys.stdout)
-        print(flask.current_app.account)
+        # print(login_sts.status_code, file=sys.stdout)
+        # print(pint.get_user_overview(), file=sys.stdout)
+        # print(flask.current_app.account)
 
         user_info= pint.get_user_overview()
         flask.current_app.user_info= user_info
@@ -54,7 +51,7 @@ def login(data=None):
         # print(current_account)
         res = []
         search_batch = current_account.search('pins', query)
-        while len(search_batch) > 0 and len(res) < 1000:
+        while len(search_batch) > 0 and len(res) < 500:
             res += search_batch
             search_batch = current_account.search('pins', query=query)
         # res=current_account.search('pins',query)
@@ -70,16 +67,22 @@ def download():
         imgs = flask.request.args.getlist('doc_imgs[]')
 
         path=query
-        if not os.path.exists(path):
-            os.mkdir(path)
+
+
+        user_name = flask.current_app.user_info['username']
+
+      
+        if not os.path.exists('Pics'):
+            os.mkdir('Pics')
         
-        
+        if not os.path.exists('Pics/'+user_name+query):
+            os.mkdir('Pics/'+user_name+'_'+query)
         
         print('downloading pictures')
         for i in imgs:
             img_name=re.search(pattern='[0-9a-z]*.jpg',string=i).group()
             # print('/'+path+'/'+img_name)  
-            req.urlretrieve(i, path+'/'+img_name)
+            req.urlretrieve(i, 'Pics/'+user_name+'_'+query+'/'+img_name)
         return  "susscess"
 
 
@@ -92,6 +95,30 @@ def account():
             json.dump(user_info, f)
     return "success"
     
+@app.route('/analysis', methods=['POST','GET'])
+def analysis():
+        if request.method == 'GET':
+            dirs=os.listdir('Pics')
+            return render_template('analysis.html',data=dirs)
+
+
+@app.route('/get_folder', methods=['GET'])
+def get_folder():
+     if request.method == 'GET':
+         folder = flask.request.args.get('name')
+         folder= folder.strip()
+         res={}
+         res['summary']=os.stat('Pics/'+folder)
+         res['array']=os.listdir('Pics/'+folder)
+
+        # So the variable res['array'] contains all the file names you need to use.
+        # You could continue coding from here
+        # This method should generate things that could possibly return to the html page (analysis.html)
+
+
+         return jsonify(result=res)
+
+
 
 
 # @app.route('/scrapper')
--- test.py
+++ test.py
@@ -3,3 +3,23 @@ a ='https://i.pinimg.com/236x/0c/f5/88/0cf5884fae3f0c9dbf5569bf17491462.jpg'
 
 
 print(re.search(pattern='(0-9a-z]*.jpg',string=a  ))
+
+
+
+import os
+
+      
+if not os.path.exists('Pics'):
+    os.mkdir('Pics')
+
+query='dogs'
+user_name='yuk'
+
+if not os.path.exists('Pics/'+user_name+query):
+    os.mkdir('Pics/'+user_name+'_'+query)
+
+os.listdir('Pics')
+
+[f for f in os.scandir() if f.is_dir()]
+
+os.listdir('Pics/'+'yukunyang_gods')
