bcc6b42d11e70a876a97097eca9a1d978d80ce7b
ReichYang <yukunyang9508@gmail.com>
2020-03-21 15:14:45 -0400

    change log in page

--- app.py
+++ app.py
@@ -397,4 +397,4 @@ def upload_file():
 if __name__ == '__main__':
     from werkzeug.serving import WSGIRequestHandler
     WSGIRequestHandler.protocol_version = "HTTP/1.1"
-    app.run(host="0.0.0.0", port=8080,debug=False, use_reloader=True)
+    app.run(host="0.0.0.0", port=8080,debug=True, use_reloader=True)
